5cec6fcd3243318b2280b35c8e0b49dd39bd0c89
Shuta-syd <shuta.ogura0921@gmail.com>
2023-01-12 20:01:00 +0900

    #10 [add] struct enable_if add

---
+++ type_traits/enable_if.hpp
@@ -0,0 +1,12 @@
+#ifndef ENABLE_IF_HPP_
+#define ENABLE_IF_HPP_
+
+namespace ft {
+	/**
+	 * @brief The type T is enabled as member type enable_if::type if Cond is true
+	 */
+	template <bool Condition, class T = void>
+	struct enable_if;
+}
+
+#endif
