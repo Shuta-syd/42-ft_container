1baebe2b82c94540db08f6a525febb3bd2bfd88a
Shuta-syd <shuta.ogura0921@gmail.com>
2023-01-13 19:10:37 +0900

    #10 [add] is_integral Implemented by inheriting from integral_constant

--- type_traits/enable_if.hpp
+++ type_traits/enable_if.hpp
@@ -5,10 +5,13 @@ namespace ft {
 	/**
 	 * @brief The type T is enabled as member type enable_if::type if Cond is true
 	 */
-	template <bool Condition, class T = void>
+	template <bool Condition, class T = void >
 	struct enable_if {
 		typedef T type;
 	};
+
+	template <typename T>
+	struct ft::enable_if<false, T> {};
 }
 
 #endif
--- type_traits/is_integral.hpp
+++ type_traits/is_integral.hpp
@@ -1,11 +1,82 @@
 #ifndef IS_INTEGRAL_HPP_
 #define IS_INTEGRAL_HPP_
 
-namespace ft {
-		
 
-	/** @brief Trait class that identifies whether T is an integral type. */
-	template <class T> struct is_integral;
+/** @brief Trait class that identifies whether T is an integral type. */
+namespace ft {
+template <class T>
+struct is_integral: public ft::is_integral_helper<T> {};
+
+	template <class T, T v>
+	struct integral_constant {
+	static constexpr T value = v;
+	typedef T value_type;
+	typedef integral_constant<T,v> type;
+	constexpr operator T() { return v; }
+};
+
+	typedef integral_constant<bool, false> false_type;
+	typedef integral_constant<bool, true> true_type;
+
+	template <typename T>
+	struct is_integral_helper : public false_type {};
+	template <>
+	struct is_integral_helper<char> : public true_type {};
+	template <>
+	struct is_integral_helper<char16_t> : public true_type {};
+	template <>
+	struct is_integral_helper<char32_t> : public true_type {};
+	template <>
+	struct is_integral_helper<wchar_t> : public true_type {};
+	template <>
+	struct is_integral_helper<signed char> : public true_type {};
+	template <>
+	struct is_integral_helper<short int> : public true_type {};
+	template <>
+	struct is_integral_helper<int> : public true_type {};
+	template <>
+	struct is_integral_helper<long int> : public true_type {};
+	template <>
+	struct is_integral_helper<long long int> : public true_type {};
+	template <>
+	struct is_integral_helper<unsigned int> : public true_type {};
+	template <>
+	struct is_integral_helper<unsigned char> : public true_type {};
+	template <>
+	struct is_integral_helper<unsigned short int> : public true_type {};
+	template <>
+	struct is_integral_helper<unsigned long int> : public true_type {};
+	template <>
+	struct is_integral_helper<unsigned long long int> : public true_type {};
+
+	template <>
+	struct is_integral_helper<const char> : public true_type {};
+	template <>
+	struct is_integral_helper<const char16_t> : public true_type {};
+	template <>
+	struct is_integral_helper<const char32_t> : public true_type {};
+	template <>
+	struct is_integral_helper<const wchar_t> : public true_type {};
+	template <>
+	struct is_integral_helper<const signed char> : public true_type {};
+	template <>
+	struct is_integral_helper<const short int> : public true_type {};
+	template <>
+	struct is_integral_helper<const int> : public true_type {};
+	template <>
+	struct is_integral_helper<const long int> : public true_type {};
+	template <>
+	struct is_integral_helper<const long long int> : public true_type {};
+	template <>
+	struct is_integral_helper<const unsigned int> : public true_type {};
+	template <>
+	struct is_integral_helper<const unsigned char> : public true_type {};
+	template <>
+	struct is_integral_helper<const unsigned short int> : public true_type {};
+	template <>
+	struct is_integral_helper<const unsigned long int> : public true_type {};
+	template <>
+	struct is_integral_helper<const unsigned long long int> : public true_type {};
 }
 
 #endif
