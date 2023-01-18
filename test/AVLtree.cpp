1b2e3fde690deceb30c2e4c5b1d2574d9da73f6c
Shuta-syd <shuta.ogura0921@gmail.com>
2023-01-18 18:47:08 +0900

    #17 [add] AVLtree::printAVL() add

--- test/AVLtree.cpp
+++ test/AVLtree.cpp
@@ -15,14 +15,27 @@ void test_avl_tree() {
 	std::pair<int, std::string> node4(21, "node4");
 	std::pair<int, std::string> node5(43, "node5");
 	std::pair<int, std::string> node6(80, "node6");
-	std::pair<int, std::string> node7(80, "node7");
-	std::pair<int, std::string> node8(80, "node8");
+	std::pair<int, std::string> node7(82, "node7");
+	std::pair<int, std::string> node8(83, "node8");
 
 	ft::AVLtree<T, std::less<int> > root_;
 	root_.insert(node1);
 	root_.insert(node2);
 	root_.insert(node3);
 	root_.insert(node4);
+	root_.insert(node5);
+	root_.insert(node6);
+	root_.insert(node7);
+	root_.insert(node8);
+
+	/**
+	 * 13
+	 *		50
+	 		42  80
+		 21 43  82
+		          83
+	 */
+	root_.printAVL(NULL, 1);
 }
 
 template <class T, class Comp>
--- utility/AVLtree.hpp
+++ utility/AVLtree.hpp
@@ -5,107 +5,140 @@
 #include <node.hpp>
 // #include <pair.hpp>
 
-namespace ft {
-	template <class T, class Comp, class Allocator= std::allocator<T> >
-	class AVLtree {
-		public:
-			typedef T value_type;
-			typedef typename value_type::first_type key_type;
-			typedef Allocator allocator_type;
-			typedef node<T> node_type;
+namespace ft
+{
+	template <class T, class Comp, class Allocator = std::allocator<T> >
+	class AVLtree
+	{
+	public:
+		typedef T value_type;
+		typedef typename value_type::first_type key_type;
+		typedef Allocator allocator_type;
+		typedef node<T> node_type;
 
-			AVLtree(): root_(), nullNode_(new node_type()), key_compare(Comp()) { root_ = nullNode_;}
-			~AVLtree() {}
+		AVLtree() : root_(), nullNode_(new node_type()), key_compare(Comp()) { root_ = nullNode_; }
+		~AVLtree() {}
 
-			void insert(value_type &val) {
-				if (root_ == nullNode_) {
-					root_ = new node_type(val, nullNode_, nullNode_);
-					return;
-				}
-
-				node_type *pta = this->searchParent(val.first);
-				if (key_compare(pta->key_, val.first)) {
-					pta->rhs_ = new node_type(val, pta, nullNode_);
-					// balanceInsert(pta->rhs_)
-				}
-				else {
-					pta->lhs_ = new node_type(val, pta, nullNode_);
-					//balanceInsert(pta->lhs_)
-				}
+		void insert(value_type &val)
+		{
+			if (root_ == nullNode_)
+			{
+				root_ = new node_type(val, nullNode_, nullNode_);
+				return;
 			}
 
-		private:
-			node_type *root_;
-			node_type *nullNode_;
-			Comp key_compare;
+			node_type *pta = this->searchParent(val.first);
+			if (key_compare(pta->key_, val.first))
+			{
+				pta->rhs_ = new node_type(val, pta, nullNode_);
+				// balanceInsert(pta->rhs_)
+			}
+			else
+			{
+				pta->lhs_ = new node_type(val, pta, nullNode_);
+				// balanceInsert(pta->lhs_)
+			}
+		}
 
+		void printAVL(node_type *node, int i)
+		{
+			if (node == NULL)
+			{
+				node = root_;
+				std::cout << "nullNode:[" << nullNode_ << "]" << std::endl;
+			}
+			std::cout << i << ":" << "[" << node->key_ << "]" << std::endl;
+			std::cout << "node:[" << node << "]" << std::endl;
+			std::cout << "lhs :[" << node->lhs_ << "]" << std::endl;
+			std::cout << "rhs :[" << node->rhs_ << "]" << std::endl;
+			if (node->lhs_ != nullNode_)
+				printAVL(node->lhs_, i + 1);
+			if (node->rhs_ != nullNode_)
+				printAVL(node->rhs_, i + 1);
+		}
 
-			/** @brief Search for appropriate parent node of val to insert by traversing from root */
-			node_type *searchParent(const key_type &key) {
-				node_type *pta = root_;
-				node_type *candidate = root_;
-				bool high_or_low;
+	private:
+		node_type *root_;
+		node_type *nullNode_;
+		Comp key_compare;
 
-				while (candidate) {
-					pta = candidate;
-					high_or_low = key_compare(pta->key_, key);
-					if ((high_or_low && pta->rhs_ == nullNode_) && (!high_or_low && pta->lhs_ == nullNode_))
-						return pta;
-					candidate = high_or_low ? pta->rhs_ : pta->lhs_;
-					if (candidate == nullNode_)
-						return pta;
+		/** @brief Search for appropriate parent node of val to insert by traversing from root */
+		node_type *searchParent(const key_type &key)
+		{
+			node_type *pta = root_;
+			node_type *candidate = root_;
+			bool high_or_low;
+
+			while (candidate)
+			{
+				pta = candidate;
+				high_or_low = key_compare(pta->key_, key);
+				if ((high_or_low && pta->rhs_ == nullNode_) && (!high_or_low && pta->lhs_ == nullNode_))
+					return pta;
+				candidate = high_or_low ? pta->rhs_ : pta->lhs_;
+				if (candidate == nullNode_)
+					return pta;
+			}
+			return pta;
+		}
+
+		/** @brief Check if it is established as an equilibrium binary tree and rotate if it is not balanced */
+		void balanceInsert(node_type *targetNode)
+		{
+			while (targetNode->pta_ != nullNode_)
+			{
+				node_type *pta = targetNode->pta_;
+				int height = pta->height_;
+
+				// targetNode inserted to left side
+				if (pta->lhs_ == targetNode)
+				{
+					if (this->bias(pta) == 2) // need to rotate
+						pta = this->bias(pta->lhs_) == 1 ? this->rotateR(pta) : this->rotateLR(pta);
+					else // -1 <= bias <= 1, no problem, update parent height
+						this->updateHeight(pta);
 				}
-				return pta;
-			}
-
-			/** @brief Check if it is established as an equilibrium binary tree and rotate if it is not balanced */
-			void balanceInsert(node_type *targetNode) {
-				while (targetNode->pta_ != nullNode_) {
-					node_type *pta = targetNode->pta_;
-					int hight = pta->height_;
-
-					// targetNode inserted to left side
-					if (pta->lhs_ == targetNode) {
-						if (this->bias(pta) == 2)// need to rotate
-							pta = this->bias(pta->lhs_) == 1 ? this->rotateR(pta) : this->rotateLR(pta);
-						else // -1 <= bias <= 1, no problem, update parent height
-							this->updateHeight(pta);
-					}
-					else { // targetNode inserted to right side
-						if (this->bias(pta) == -2)// need to rotate
-							pta = this->bias(pta->lhs_) == -1 ? this->rotateL(pta) : this->rotateRL(pta);
-						else // -1 <= bias <= 1, no problem, update parent height
-							this->updateHeight(pta);
-					}
-					targetNode = pta;
+				else
+				{														 // targetNode inserted to right side
+					if (this->bias(pta) == -2) // need to rotate
+						pta = this->bias(pta->lhs_) == -1 ? this->rotateL(pta) : this->rotateRL(pta);
+					else // -1 <= bias <= 1, no problem, update parent height
+						this->updateHeight(pta);
 				}
+				if (height == pta->height_) // nothing update == end point
+					break;
+				targetNode = pta;
 			}
+		}
 
-			/** @brief single left rotate */
-			void rotateL() {}
-			/** @brief single right rotate */
-			void rotateR() {}
-			/** @brief single RR rotate + LL rotate */
-			void rotateRL() {}
-			/** @brief single LL rotate + RR rotate rotate */
-			void rotateLR() {}
+		/** @brief single left rotate */
+		void rotateL() {}
+		/** @brief single right rotate */
+		void rotateR() {}
+		/** @brief single RR rotate + LL rotate */
+		void rotateRL() {}
+		/** @brief single LL rotate + RR rotate rotate */
+		void rotateLR() {}
 
-			/**
-			 * @brief Calculate the difference (bias) between left and right nodes
-			 * 				Formula is: left partial tree - right partial tree
-			 */
-			int bias(node_type *node) {
-				return (node->lhs->height - node->rhs->height);
-			}
+		/**
+		 * @brief Calculate the difference (bias) between left and right nodes
+		 * 				Formula is: left partial tree - right partial tree
+		 */
+		int bias(node_type *node)
+		{
+			return (node->lhs->height - node->rhs->height);
+		}
 
-			/** @brief Update node height and bias by insertion or deletion */
-			void updateHeight(node_type *node) {
-				int left_height = node->lhs_->height_;
-				int right_height = node->rhs_->height_;
+		/** @brief Update node height and bias by insertion or deletion */
+		void updateHeight(node_type *node)
+		{
+			int left_height = node->lhs_->height_;
+			int right_height = node->rhs_->height_;
 
-				node->height_ = 1 + (left_height > right_height ? left_height : right_height);
-				node->bias_ = left_height - right_height;
-			}
+			node->height_ = 1 + (left_height > right_height ? left_height : right_height);
+			node->bias_ = left_height - right_height;
+		}
+
 	};
 }
 
